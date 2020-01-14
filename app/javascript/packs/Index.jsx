321fbbaa8b5c484ee0923c2ee4a007342f52105c
shadowezz <wenjie.cao@hotmail.com>
2020-01-14 19:07:50 +0800

    fix bootstrap styling deployment errors

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,12 +4,11 @@
 
 import React from 'react'
 import { render } from "react-dom";
-import 'bootstrap'
-import 'stylesheets/application'
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'stylesheets/custom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
 
